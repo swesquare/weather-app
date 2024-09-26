77e3ea15d57f94062b4edc6cd0a13497708dcaf7
Swe Swe <sweswe163@gmail.com>
2024-09-26 21:57:16 +0630

    Add icon feature

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  timeElement.innerHTML = formatDate(date);
+  let iconElement = document.querySelector("#icon");
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-
+  iconElement.innerHTML = `<img src="${respone.data.condition.icon_url}" class="weather-app-icon" /> `;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 function formatDate(date) {
